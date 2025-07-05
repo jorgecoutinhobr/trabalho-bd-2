f9e0112ff07c1f0354204c778375bd2f1c4c0493
Jorge Coutinho <jorgecsn@id.uff.br>
2025-07-05 20:18:49 -0300

    Adiciona novas consultas e índices para otimização de desempenho no banco de dados

--- scripts/05_consultas.sql
+++ scripts/05_consultas.sql
@@ -25,3 +25,72 @@ ORDER BY
     aj.data_avaliacao DESC;
 ----
 
+-- CONSULTA 2: Relatório de desempenho por editora
+EXPLAIN ANALYZE SELECT
+    e.nome_editora,
+    COUNT(DISTINCT j.id_jogo) AS total_jogos_publicados,
+    AVG(av.nota) AS media_geral_avaliacoes,
+    COUNT(ju.id_jogo) AS total_copias_na_plataforma
+FROM
+    editoras e
+JOIN
+    jogos j ON e.id_editora = j.id_editora
+LEFT JOIN
+    avaliacoes_jogo av ON j.id_jogo = av.id_jogo
+LEFT JOIN
+    jogos_usuario ju ON j.id_jogo = ju.id_jogo
+GROUP BY
+    e.id_editora, e.nome_editora
+ORDER BY
+    total_copias_na_plataforma DESC;
+--
+
+-- -- CONSULTA 3: Sugerir jogos para um usuário alvo (ID = 123)
+EXPLAIN ANALYZE
+WITH JogosDoUsuarioAlvo AS (
+    SELECT id_jogo FROM jogos_usuario WHERE id_usuario = 123
+),
+UsuariosSimilares AS (
+    SELECT DISTINCT ju.id_usuario
+    FROM jogos_usuario ju
+    WHERE ju.id_jogo IN (SELECT id_jogo FROM JogosDoUsuarioAlvo)
+      AND ju.id_usuario != 123
+)
+
+SELECT
+    j.titulo,
+    COUNT(ju.id_usuario) AS popularidade_entre_similares
+FROM
+    jogos_usuario ju
+JOIN
+    jogos j ON ju.id_jogo = j.id_jogo
+WHERE
+    ju.id_usuario IN (SELECT id_usuario FROM UsuariosSimilares)
+    AND ju.id_jogo NOT IN (SELECT id_jogo FROM JogosDoUsuarioAlvo)
+GROUP BY
+    j.id_jogo, j.titulo
+ORDER BY
+    popularidade_entre_similares DESC
+LIMIT 15;
+--
+
+-- Consulta 4:  Encontrar usuários que têm o jogo base mas não a DLC
+EXPLAIN ANALYZE SELECT
+    u.id_usuario,
+    u.email
+FROM
+    usuarios u
+JOIN
+    jogos_usuario ju_base ON u.id_usuario = ju_base.id_usuario AND ju_base.id_jogo = 45)
+WHERE
+    NOT EXISTS (
+        SELECT 1
+        FROM jogos_usuario ju_dlc
+        WHERE ju_dlc.id_usuario = u.id_usuario
+          AND ju_dlc.id_jogo = (SELECT d.id_dlc FROM dlcs d WHERE d.id_dlc = 12)
+    );
+--
+
+-- Consulta 5
+    
+--
--- scripts/06_indices.sql
+++ scripts/06_indices.sql
@@ -6,4 +6,17 @@ CREATE INDEX IF NOT EXISTS idx_jogo_categoria_id_categoria ON jogo_categoria (id
 --
 
 -- indices consulta 2
+CREATE INDEX IF NOT EXISTS idx_jogos_id_editora ON jogos (id_editora);
+CREATE INDEX IF NOT EXISTS idx_avaliacoes_jogo_id_jogo ON avaliacoes_jogo (id_jogo);
+CREATE INDEX IF NOT EXISTS idx_jogos_usuario_id_jogo ON jogos_usuario (id_jogo);
 --
+
+-- indices consulta 3
+CREATE INDEX idx_jogos_usuario_id_usuario_id_jogo ON jogos_usuario (id_usuario, id_jogo);
+CREATE INDEX idx_jogos_usuario_id_jogo_id_usuario ON jogos_usuario (id_jogo, id_usuario);
+--
+
+-- indices consulta 4
+CREATE INDEX IF NOT EXISTS idx_jogos_usuario_id_usuario_id_jogo ON jogos_usuario (id_usuario, id_jogo);
+CREATE INDEX IF NOT EXISTS idx_jogos_usuario_id_jogo_id_usuario ON jogos_usuario (id_jogo, id_usuario);
+-- 
