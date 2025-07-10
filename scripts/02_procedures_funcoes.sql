fad871657a7577c1b63a23534a76b3038d232a20
Higor Abreu <higorabreu@id.uff.br>
2025-07-10 17:39:25 -0300

    adicionado uma procedure

--- scripts/02_procedures_funcoes.sql
+++ scripts/02_procedures_funcoes.sql
@@ -1,3 +1,4 @@
+-- Função para calcular o preço de um jogo com desconto percentual
 CREATE OR REPLACE FUNCTION fn_calcular_preco_com_desconto(id_jogo_param INT)
 RETURNS DECIMAL(10,2) AS $$
 DECLARE
@@ -24,3 +25,30 @@ BEGIN
     RETURN ROUND(preco_final, 2);
 END;
 $$ LANGUAGE plpgsql;
+
+-- Função para criar um ticket de suporte
+CREATE OR REPLACE PROCEDURE sp_criar_ticket_suporte(
+    p_id_remetente INT,
+    p_assunto VARCHAR(255),
+    p_descricao TEXT,
+    p_prioridade VARCHAR(20)
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_id_ticket INT;
+BEGIN
+    BEGIN
+        INSERT INTO tickets_suporte (assunto, descricao, status, data_abertura, data_ultima_atualizacao, prioridade)
+        VALUES (p_assunto, p_descricao, 'Aberto', NOW(), NOW(), p_prioridade)
+        RETURNING id_ticket INTO v_id_ticket;
+
+        INSERT INTO mensagens_ticket (id_ticket, id_remetente, conteudo, data_envio)
+        VALUES (v_id_ticket, p_id_remetente, p_descricao, NOW());
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE NOTICE 'Ocorreu um erro ao criar o ticket de suporte. A transação foi revertida.';
+            RAISE;
+    END;
+END;
+$$;
